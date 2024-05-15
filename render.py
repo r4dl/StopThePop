de903512bae1113e7970d1091cf5fe2735b2ad75
Michael <steimich96@gmail.com>
2024-05-15 10:55:34 +0200

    Removed sorted in render

--- render.py
+++ render.py
@@ -29,7 +29,7 @@ def render_set(model_path, name, iteration, views, gaussians, pipeline, backgrou
     makedirs(gts_path, exist_ok=True)
 
     for idx, view in enumerate(tqdm(views, desc="Rendering progress")):
-        rendering = render(view, gaussians, pipeline, background, sorted=sorted, per_tile_depth=per_tile_depth, sort_window=sort_window)["render"]
+        rendering = render(view, gaussians, pipeline, background, per_tile_depth=per_tile_depth, sort_window=sort_window)["render"]
         gt = view.original_image[0:3, :, :]
         torchvision.utils.save_image(rendering, os.path.join(render_path, '{0:05d}'.format(idx) + ".png"))
         torchvision.utils.save_image(gt, os.path.join(gts_path, '{0:05d}'.format(idx) + ".png"))
