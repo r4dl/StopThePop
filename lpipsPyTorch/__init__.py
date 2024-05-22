69d27c7e31b3d4adbef8188ae4f8a336851b387a
Lukas Radl <lukas.radl@icg.tugraz.at>
2024-05-22 16:32:52 +0200

    this one was missing too

--- lpipsPyTorch/__init__.py
+++ lpipsPyTorch/__init__.py
@@ -2,20 +2,23 @@ import torch
 
 from .modules.lpips import LPIPS
 
-
-def lpips(x: torch.Tensor,
-          y: torch.Tensor,
-          net_type: str = 'alex',
-          version: str = '0.1'):
-    r"""Function that measures
-    Learned Perceptual Image Patch Similarity (LPIPS).
+class LPIPSEval:
+    r"""Class for measuring lpips between two images
 
     Arguments:
-        x, y (torch.Tensor): the input tensors to compare.
         net_type (str): the network type to compare the features: 
                         'alex' | 'squeeze' | 'vgg'. Default: 'alex'.
         version (str): the version of LPIPS. Default: 0.1.
     """
-    device = x.device
-    criterion = LPIPS(net_type, version).to(device)
-    return criterion(x, y)
+    criterion = None
+    
+    def __init__(self, device, net_type: str = 'alex', version: str = '0.1'):
+        self.criterion = LPIPS(net_type, version).to(device)
+        
+    r"""Compute lpips for two images
+
+    Arguments:
+        x, y (torch.Tensor): the input tensors to compare.
+    """
+    def eval(self, x: torch.Tensor, y: torch.Tensor):
+        return self.criterion(x, y)
